8bffc30cd20e34f5117c2f7d08d6322989459eef
Ryan Wahle <ryanwahle@fullsail.edu>
2013-02-14 20:00:28 -0700

    Finished the display data function

--- js/main.js
+++ js/main.js
@@ -42,20 +42,42 @@ function displayData ()
 	var localStorageLength = localStorage.length;
 	
 	if (localStorageLength > 0) {
+		// Hide the form, hide the display data link, and show the add tv show button
+		$("addTVShowForm").style.display = "none";
+		$("displayDataLink").style.display = "none";
+		$("addTVShowLink").style.display = "inline";
+		
+		// Create the new html so we can display the data
 		for (var i = 0; i < localStorageLength; i++) {
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
 			var myTVShow = JSON.parse(value);
 		
-			var tagTVShowData = document.createElement("p");
-			tagTVShowData.innerHTML = myTVShow.showName;
+			var tagTVShowList = document.createElement("ul");
 			
-			$("mainSection").appendChild(tagTVShowData);
+			tagTVShowList.appendChild(createElementLI("Show Name: " + myTVShow.showName));
+			tagTVShowList.appendChild(createElementLI("Day of Week: " + myTVShow.dayOfWeek));
+			tagTVShowList.appendChild(createElementLI("Time: " + myTVShow.time));
+			tagTVShowList.appendChild(createElementLI("Favorite: " + myTVShow.favorite));
+			tagTVShowList.appendChild(createElementLI("Rating: " + myTVShow.rating));
+			tagTVShowList.appendChild(createElementLI("Starting Date: " + myTVShow.startingDate));
+			tagTVShowList.appendChild(createElementLI("Description: " + myTVShow.description));
 			
+			
+			// Put it all the added HTML to the page!
+			$("mainSection").appendChild(tagTVShowList);
 		}
 	} else {
 		alert("Please add a TV Show first.");
 	}
+}
+
+function createElementLI (text)
+{
+	var tag = document.createElement("li");
+	tag.innerHTML = text;
+	
+	return tag;
 }
 
 //alert(localStorage.length);
