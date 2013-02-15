7c88ec547d0d3170d8dc4ba4b54f95605896207c
Ryan Wahle <ryanwahle@fullsail.edu>
2013-02-14 19:17:22 -0700

    Added the clearStoredData function and the beginnings of displaying data

--- js/main.js
+++ js/main.js
@@ -2,21 +2,70 @@
 /* VFW 1302   */
 /* Project 2  */
 
-function addTVShow () {
+function $ (element)
+{
+	return document.getElementById(element);
+}
+
+function addTVShow ()
+{
 	var myTVShow = {
-			showName: document.getElementById("showName").value,
-			dayOfWeek: document.getElementById("dayOfWeek").value,
-			time: document.getElementById("time").value,
-			favorite: document.getElementById("favorite").checked,
-			rating: document.getElementById("rating").value,
-			startingDate: document.getElementById("startingDate").value,
-			description: document.getElementById("description").value
+			showName: $("showName").value,
+			dayOfWeek: $("dayOfWeek").value,
+			time: $("time").value,
+			favorite: $("favorite").checked,
+			rating: $("rating").value,
+			startingDate: $("startingDate").value,
+			description: $("description").value
 	};
 	
 	var key = Math.floor(Math.random()*10000000001);
-	localStorage.setItem(key, JSON.stringify(myTVShow)); //JSON.parse to get back
+	localStorage.setItem(key, JSON.stringify(myTVShow));
+}
+
+function clearStoredData ()
+{
+	if (localStorage.length === 0) {
+		alert("There is no data to clear!");
+	} else {
+		var clearStoredDataConfirmation = confirm("Are you sure you want to clear all the data?");
+		
+		if (clearStoredDataConfirmation) {
+			localStorage.clear();
+			window.location.reload();
+		}
+	}
+}
+
+function displayData ()
+{
+	var localStorageLength = localStorage.length;
+	
+	if (localStorageLength > 0) {
+		for (var i = 0; i < localStorageLength; i++) {
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			var myTVShow = JSON.parse(value);
+		
+			var tagTVShowData = document.createElement("p");
+			tagTVShowData.innerHTML = myTVShow.showName;
+			
+			$("mainSection").appendChild(tagTVShowData);
+			
+		}
+	} else {
+		alert("Please add a TV Show first.");
+	}
 }
 
 //alert(localStorage.length);
-var addTVShowButton = document.getElementById("addTVShowButton");
+
+// Add some event listeners
+var addTVShowButton = $("addTVShowButton");
+var clearStoredDataLink = $("clearStoredDataLink");
+var displayDataLink = $("displayDataLink");
+
 addTVShowButton.addEventListener("click", addTVShow);
+clearStoredDataLink.addEventListener("click", clearStoredData);
+displayDataLink.addEventListener("click", displayData);
+
